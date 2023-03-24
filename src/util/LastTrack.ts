7d96447ac658e9907ade33637f705e3c22d0ce65
Timo <timo.nock@stud.h-da.de>
2023-03-24 20:47:18 +0100

    make website

---
+++ src/util/LastTrack.ts
@@ -0,0 +1,9 @@
+export interface LastTrack {
+  artist: string;
+  image: string;
+  album: string;
+  name: string;
+  nowplaying: boolean;
+  url: string;
+  altText: string;
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        emanon: "url('/emanon.webp')"
+      },
+      colors: {
+        blackAcrylic: "rgba(0, 0, 0, 0.5)"
+      }
+    }
+  },
+  plugins: []
+};
